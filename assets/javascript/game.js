748b3527cdc17c61c999f1f3522cf342671fad37
Smriti Sri <smritis2001@gmail.com>
2019-05-04 14:32:22 -0500

    adding files

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,29 +1,72 @@
-$(document).ready(function(){//I need jQuery to run!
-	var goal, current, redNumber, blueNumber, yellowNumber, greenNumber;
+$(document).ready(function () {//I need jQuery to run!
+    var goal, current, win, loss, redNumber, blueNumber, yellowNumber, greenNumber;
 
-	var resetGame = function(){
-		goal = Math.floor(Math.random()*101 + 19);
-		current = 0;
-		redNumber = Math.floor(Math.random()*12);
-		blueNumber = Math.floor(Math.random()*12);
-		yellowNumber = Math.floor(Math.random()*12);
-		greenNumber = Math.floor(Math.random()*12);
-	}
+    var resetGame = function () {
+        goal = Math.floor(Math.random() * 101 + 19);
+        current = 0;
+        win = 0;
+        loss = 0;
+        redNumber = Math.floor(Math.random() * 12);
+        blueNumber = Math.floor(Math.random() * 12);
+        yellowNumber = Math.floor(Math.random() * 12);
+        greenNumber = Math.floor(Math.random() * 12);
 
-	resetGame();
- 
-	
+        $('#goal').text(goal);
+    $('#current').text(current);
+    $("#button-red").attr('value', redNumber);
+    $("#button-blue").attr('value', blueNumber);
+    $("#button-yellow").attr('value', yellowNumber);
+    $("#button-green").attr('value', greenNumber);
+    }
 
-	$("#button-red").attr('value', redNumber);
-	$("#button-blue").attr('value', blueNumber);
-	$("#button-yellow").attr('value', yellowNumber);
-	$("#button-green").attr('value', greenNumber);
+    resetGame();
+
+    
 
 
-	$('.crystal-button').click(function(){
-		current += parseInt($(this).attr('value'));
-		console.log(current);
-	});
+    $('#button-red').click(function () {
+        current += parseInt($(this).attr('value'));
+        console.log(current);
+        $('#current').text(current);
+        check();
+    });
+
+    $('#button-blue').click(function () {
+        current += parseInt($(this).attr('value'));
+        console.log(current);
+        $('#current').text(current);
+        check();
+    });
+
+    $('#button-yellow').click(function () {
+        current += parseInt($(this).attr('value'));
+        console.log(current);
+        $('#current').text(current);
+        check();
+    });
+
+    $('#button-green').click(function () {
+        current += parseInt($(this).attr('value'));
+        console.log(current);
+        $('#current').text(current);
+        check();
+    });
+
+    function check() {
+        if (goal === current) {
+            win++;
+            $('#win').text(win);
+            console.log(win);
+            resetGame();
+        }
+        if (current > goal) {
+            loss++;
+            console.log('loss', loss);
+            $('#loss').text(loss);
+            resetGame();
+        }
+    }
+
 
 
 
